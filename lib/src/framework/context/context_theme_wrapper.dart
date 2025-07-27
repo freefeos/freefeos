94cf6b6f3f008f06874e4bebc01531ca51b9cff7
wyq0918dev <wyq0918dev@gmail.com>
2025-07-27 21:09:25 +0800

    commit

--- lib/src/framework/context/context_theme_wrapper.dart
+++ lib/src/framework/context/context_theme_wrapper.dart
@@ -3,4 +3,9 @@ part of '../framework.dart';
 abstract base class ContextThemeWrapper extends StatefulWidget
     with ContextWrapper {
   const ContextThemeWrapper({super.key});
+
+  @protected
+  @factory
+  @override
+  ContextStateWrapper createState();
 }
