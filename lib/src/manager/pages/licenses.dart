46fbe0fe77797d7a13b54b2dc477d013f7d5a7fc
wyq0918dev <wyq0918dev@gmail.com>
2025-08-30 20:27:18 +0800

    commit

--- lib/src/manager/pages/licenses.dart
+++ lib/src/manager/pages/licenses.dart
@@ -12,11 +12,29 @@ final class LicensesPage extends UiPage {
 final class _LicensesPageState extends State<LicensesPage> {
   _LicensesPageState();
 
+  /// 应用信息
+  PackageInfo? _packageInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    PackageInfo.fromPlatform().then((res) {
+      setState(() => _packageInfo = res);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // 清空应用信息
+    _packageInfo = null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return LicensePage(
-      applicationName: 'appName',
-      applicationVersion: 'appVersionName',
+      applicationName: _packageInfo?.appName ?? 'Unknown',
+      applicationVersion: _packageInfo?.version ?? 'Unknown',
       applicationLegalese: 'Powered by FreeFEOS',
     );
   }
