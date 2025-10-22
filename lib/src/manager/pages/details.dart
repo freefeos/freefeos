208187b520f6f41d1d36c3eef0226fedec949644
wyq0918dev <wyq0918dev@gmail.com>
2025-10-22 22:46:53 +0800

    commit

--- lib/src/manager/pages/details.dart
+++ lib/src/manager/pages/details.dart
@@ -37,53 +37,125 @@ final class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('详细信息')),
-      body: Consumer<DetailsViewModule>(
-        builder: (context, dvm, child) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 32,
-                  top: 16,
-                  right: 32,
-                  bottom: 8,
-                ),
-                child: Text(
-                  '基础信息',
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
+      body: Scrollbar(
+        controller: _scrollController,
+        child: ScreenAdapter(
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(
+              context,
+            ).copyWith(scrollbars: false),
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              padding: EdgeInsets.zero,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 32,
+                      top: 16,
+                      right: 32,
+                      bottom: 8,
+                    ),
+                    child: Text(
+                      '基础信息',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Text('应用名称: ${_packageInfo?.appName ?? 'Unknown'}'),
-              Text('版本名称: ${_packageInfo?.version ?? 'Unknown'}'),
-              Text('内部构建号: ${_packageInfo?.buildNumber ?? 'Unknown'}'),
-              Text('应用包名: ${_packageInfo?.packageName ?? 'Unknown'}'),
-              Text(
-                '首次安装时间: ${dvm.formattDateString(_packageInfo?.installTime) ?? 'Unknown'}',
-              ),
-              Text(
-                '最后更新时间: ${dvm.formattDateString(_packageInfo?.updateTime) ?? 'Unknown'}',
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 32,
-                  top: 8,
-                  right: 32,
-                  bottom: 8,
-                ),
-                child: Text(
-                  '服务声明',
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
+                  Consumer<DetailsViewModule>(
+                    builder: (context, dvm, child) {
+                      return Card.filled(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              title: Text('应用名称'),
+                              subtitle: Text(
+                                _packageInfo?.appName ?? 'Unknown',
+                              ),
+                            ),
+                            const Divider(height: 1, indent: 16, endIndent: 16),
+                            ListTile(
+                              title: Text('版本名称'),
+                              subtitle: Text(
+                                _packageInfo?.version ?? 'Unknown',
+                              ),
+                            ),
+                            const Divider(height: 1, indent: 16, endIndent: 16),
+                            ListTile(
+                              title: Text('内部构建号'),
+                              subtitle: Text(
+                                _packageInfo?.buildNumber ?? 'Unknown',
+                              ),
+                            ),
+                            const Divider(height: 1, indent: 16, endIndent: 16),
+                            ListTile(
+                              title: Text('应用包名'),
+                              subtitle: Text(
+                                _packageInfo?.packageName ?? 'Unknown',
+                              ),
+                            ),
+                            const Divider(height: 1, indent: 16, endIndent: 16),
+                            ListTile(
+                              title: Text('首次安装时间'),
+                              subtitle: Text(
+                                dvm.formattDateString(
+                                      _packageInfo?.installTime,
+                                    ) ??
+                                    'Unknown',
+                              ),
+                            ),
+                            const Divider(height: 1, indent: 16, endIndent: 16),
+                            ListTile(
+                              title: Text('最后更新时间'),
+                              subtitle: Text(
+                                dvm.formattDateString(
+                                      _packageInfo?.updateTime,
+                                    ) ??
+                                    'Unknown',
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 32,
+                      top: 8,
+                      right: 32,
+                      bottom: 8,
+                    ),
+                    child: Text(
+                      '服务声明',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                  Card.filled(
+                    margin: EdgeInsets.only(
+                      left: 16,
+                      right: 16,
+                      top: 8,
+                      bottom: 16,
+                    ),
+                    child: ListTile(
+                      leading: Icon(Icons.warning_amber),
+                      title: Text('由于使用此软件库的开发者的个人行为导致的违法违规, 与此软件库开发者无关.'),
+                    ),
+                  ),
+                ],
               ),
-              Text('由于使用此软件库的开发者的个人行为导致的违法违规, 与此软件库开发者无关.'),
-            ],
-          );
-        },
+            ),
+          ),
+        ),
       ),
     );
   }
